6200ffe19ab82be1a50ebc2a7f34bfa50db1056d
SparkJiao <jiaofangkai@hotmail.com>
2019-11-07 00:20:49 +0800

    * modified hyperparameter

--- SUMBT/run-multiwoz2-data2.0-share-tf.sh
+++ SUMBT/run-multiwoz2-data2.0-share-tf.sh
@@ -4,7 +4,7 @@ output_dir=exp-multiwoz/exp-data2.0-share-tf1.0
 target_slot='all'
 bert_dir='/home/jiaofangkai/'
 
-python3 code/main-multislot-share.py --do_train --do_eval --num_train_epochs 10 --data_dir data/multiwoz2.0 \
+python3 code/main-multislot-share.py --do_train --do_eval --num_train_epochs 20 --data_dir data/multiwoz2.0 \
 --bert_model bert-base-uncased --do_lower_case --bert_dir $bert_dir --task_name bert-gru-sumbt \
 --nbt transformer --output_dir $output_dir --target_slot all --warmup_proportion 0.1 --learning_rate 5e-5 \
 --train_batch_size 8 --eval_batch_size 1 --distance_metric product --patience 10 \
