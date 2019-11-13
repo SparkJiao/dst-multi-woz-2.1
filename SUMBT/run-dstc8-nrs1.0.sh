3fb45809a67346e05f39837289edeba96d9c2f19
SparkJiao <jiaofangkai@hotmail.com>
2019-11-14 00:42:28 +0800

    Add fine-tune scripts

--- SUMBT/run-dstc8-nrs1.0.sh
+++ SUMBT/run-dstc8-nrs1.0.sh
@@ -10,7 +10,7 @@ python code/main-share-nrs.py --do_train --do_eval --num_train_epochs 1 --data_d
 --nbt transformer --output_dir $output_dir --warmup_proportion 0.1 --learning_rate 5e-5 --target_slot all \
 --train_batch_size 64 --eval_batch_size 8 --distance_metric product --patience 10 \
 --tf_dir tensorboard/dstc/ --max_seq_length 128 --max_turn_length 22 \
---fp16 --fp16_opt_level O2 --gradient_accumulation_steps 8 \
+--fp16 --fp16_opt_level O2 --gradient_accumulation_steps 16 \
 --reduce_layers 2 --num_rnn_layers 4 --max_sample_length 64 \
 --train_file $train_file --dev_file $dev_file
 
