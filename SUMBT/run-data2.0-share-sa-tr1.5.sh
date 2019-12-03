d148fd480b43564b79e23c7307bfc2100a0a4ae4
SparkJiao <jiaofangkai@hotmail.com>
2019-12-03 23:47:41 +0800

    Update * change hyper-parameters

--- SUMBT/run-data2.0-share-sa-tr1.5.sh
+++ SUMBT/run-data2.0-share-sa-tr1.5.sh
@@ -14,7 +14,7 @@ target_slot='all'
 nbt='transpose'
 bert_dir='/home/jiaofangkai/'
 
-python code/main-multislot-share.py --do_train --do_eval --num_train_epochs 8 --data_dir data/multiwoz2.0 \
+python code/main-multislot-share.py --do_train --do_eval --num_train_epochs 10 --data_dir data/multiwoz2.0 \
 --bert_model bert-base-uncased --do_lower_case --bert_dir $bert_dir --task_name bert-gru-sumbt \
 --nbt $nbt --output_dir $output_dir --target_slot all --warmup_proportion 0.1 --learning_rate 4e-5 \
 --train_batch_size 1 --eval_batch_size 1 --distance_metric product --patience 5 \
