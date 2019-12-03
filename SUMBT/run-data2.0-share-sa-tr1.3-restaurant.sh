5351d2563b179016b1b14080790e872b837b610a
SparkJiao <jiaofangkai@hotmail.com>
2019-12-03 14:47:19 +0800

    Update * add new running scripts

--- SUMBT/run-data2.0-share-sa-tr1.3-restaurant.sh
+++ SUMBT/run-data2.0-share-sa-tr1.3-restaurant.sh
@@ -6,16 +6,26 @@
 #output_dir=exp-multiwoz/data2.0-share-sa1.2  # gradient accumulation steps 8 -> 1 .epoch 6 -> 10
 #output_dir=exp-multiwoz/data2.0-share-sa1.3 # epoch 10 -> 6  --sa_add_layer_norm
 #output_dir=exp-multiwoz/data2.0-share-sa-tr1.0  # --override_attn
-output_dir=exp-multiwoz/data2.0-share-sa-tr1.1  # transpose_layer 1 -> 3  max_seq_length 64 -> 96
-target_slot='all'
+output_dir=exp-multiwoz/data2.0-share-sa-tr1.3-restaurant  # transpose_layer 1 -> 3  max_seq_length 64 -> 96
+target_slot='restaurant'
 nbt='transpose'
 bert_dir='/home/jiaofangkai/'
 
 python code/main-multislot-share.py --do_train --do_eval --num_train_epochs 6 --data_dir data/multiwoz2.0 \
 --bert_model bert-base-uncased --do_lower_case --bert_dir $bert_dir --task_name bert-gru-sumbt \
---nbt $nbt --output_dir $output_dir --target_slot all --warmup_proportion 0.1 --learning_rate 5e-5 \
+--nbt $nbt --output_dir $output_dir --target_slot $target_slot --warmup_proportion 0.1 --learning_rate 5e-5 \
 --train_batch_size 1 --eval_batch_size 1 --distance_metric product --patience 5 \
 --tf_dir tensorboard --max_seq_length 96 --max_turn_length 22 \
 --fp16 --fp16_opt_level O2 --gradient_accumulation_steps 1 \
 --reduce_layers 0 --max_label_length 17 --max_slot_length 6 \
---override_attn --transpose_layer 3
+--override_attn --transpose_layer 4
+
+target_slot='all'
+python code/main-multislot-share.py --do_eval --num_train_epochs 6 --data_dir data/multiwoz2.0 \
+--bert_model bert-base-uncased --do_lower_case --bert_dir $bert_dir --task_name bert-gru-sumbt \
+--nbt $nbt --output_dir $output_dir --target_slot $target_slot --warmup_proportion 0.1 --learning_rate 5e-5 \
+--train_batch_size 1 --eval_batch_size 1 --distance_metric product --patience 5 \
+--tf_dir tensorboard --max_seq_length 96 --max_turn_length 22 \
+--fp16 --fp16_opt_level O2 --gradient_accumulation_steps 1 \
+--reduce_layers 0 --max_label_length 17 --max_slot_length 6 \
+--override_attn --transpose_layer 4
