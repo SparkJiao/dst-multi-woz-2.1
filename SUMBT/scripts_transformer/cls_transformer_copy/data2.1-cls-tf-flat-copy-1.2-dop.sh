2272d5a5e1aeea217a86bc42523aedc040b3f5f1
SparkJiao <jiaofangkai@hotmail.com>
2020-08-07 11:00:00 +0800

    Update * Add double optimizer running scripts

--- SUMBT/scripts_transformer/cls_transformer_copy/data2.1-cls-tf-flat-copy-1.2-dop.sh
+++ SUMBT/scripts_transformer/cls_transformer_copy/data2.1-cls-tf-flat-copy-1.2-dop.sh
@@ -8,8 +8,8 @@ nbt='copy'
 bert_dir='/home/jiaofangkai/bert-base-uncased'
 bert_name='bert-base-uncased'
 
-python -m torch.distributed.launch --nproc_per_node 2 \
-code_transformer/main_torch_1_6_distribute_dop.py --do_train --do_eval --num_train_epochs 20 --data_dir data/multiwoz2.1_5 \
+python \
+code_transformer/main_torch_1_6_dop.py --do_train --do_eval --num_train_epochs 20 --data_dir data/multiwoz2.1_5 \
 --bert_model bert-base-uncased --do_lower_case --bert_dir $bert_dir --task_name bert-gru-sumbt --bert_name ${bert_name} \
 --nbt $nbt --output_dir $output_dir --target_slot $target_slot --warmup_proportion 0.1 --learning_rate 5e-5 \
 --train_batch_size 4 --eval_batch_size 1 --distance_metric euclidean --patience 15 \
@@ -20,7 +20,7 @@ code_transformer/main_torch_1_6_distribute_dop.py --do_train --do_eval --num_tra
 --dev_file data/multiwoz2.1_5/dev-5-full-value.tsv \
 --test_file data/multiwoz2.1_5/test-5-full-value.tsv \
 --ontology data/multiwoz2.1_5/ontology-full.json \
---cls_type 0 --value_embedding_type cls --intermediate_size 1536 --per_eval_steps 500 --add_interaction --efficient --sinusoidal_embeddings \
+--cls_type 0 --value_embedding_type cls --intermediate_size 1536 --per_eval_steps 1000 --add_interaction --sinusoidal_embeddings \
 --op2_lr 1e-4
 
 #CUDA_VISIBLE_DEVICES=0 python \
